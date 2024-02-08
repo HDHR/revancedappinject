49c5082ba81a0bdcdcf1775d9d5a336ce665b8c8
Harist Dani Hisyam <23426661+HDHR@users.noreply.github.com>
2024-02-08 09:46:27 +0700

    Fix module support

--- customize.sh
+++ customize.sh
@@ -2,13 +2,11 @@
 # Abort TWRP installation with error message when user tries to install this module in TWRP
 
 if [ $BOOTMODE = false ]; then
-	ui_print "- Installing through TWRP Not supported"
-	ui_print "- Intsall this module via Magisk Manager"
+	ui_print "- Installing through TWRP Not supported!"
+	ui_print "- Install this module via APatch/KSU/Magisk Manager"
 	abort "- Aborting installation !!"
 fi
 api_level_arch_detect
-
-MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 
 PKGNAME="$(grep_prop package "$MODPATH/module.prop")"
 STOCKAPPVER=$(dumpsys package $PKGNAME | grep versionName | cut -d= -f 2 | sed -n '1p')
@@ -21,7 +19,7 @@ then
 	abort "- Aborting installation !!"
 fi
 
-[ ! -d "$MAGISKTMP/.magisk/modules/magisk_proc_monitor" ] && {
+[ ! -d "/data/adb/modules/magisk_proc_monitor" ] && {
     MURL=http://github.com/HuskyDG/magisk_proc_monitor
     ui_print "- Process monitor tool is not installed"
     ui_print "  Please install it from $MURL"
